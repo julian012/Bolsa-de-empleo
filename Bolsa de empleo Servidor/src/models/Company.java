309abfcbc2ec7af25250197b43eceb8541e51686
julian012 <julicamnet@gmail.com>
2017-12-09 13:33:28 -0500

    Actualizado

--- Bolsa de empleo Servidor/src/models/Company.java
+++ Bolsa de empleo Servidor/src/models/Company.java
@@ -1,5 +1,51 @@
 package models;
 
-public class Company {
+import java.util.Stack;
 
+public class Company extends Account{
+
+	private int idCompany;
+	private String nameCompany;
+	private String description;
+	private Stack<NotificationCompany> notificationList;
+	public Company(String email, String password, String namePhoto, String numberPhone, String address, City city,
+			int idCompany, String nameCompany, String description, Stack<NotificationCompany> notificationList) {
+		super(email, password, namePhoto, numberPhone, address, city);
+		setIdCompany(idCompany);
+		setNameCompany(nameCompany);
+		setDescription(description);
+		setNotificationList(notificationList);
+	}
+	
+	public int getIdCompany() {
+		return idCompany;
+	}
+	
+	private void setIdCompany(int idCompany) {
+		this.idCompany = idCompany;
+	}
+	
+	public String getNameCompany() {
+		return nameCompany;
+	}
+	
+	public void setNameCompany(String nameCompany) {
+		this.nameCompany = nameCompany;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Stack<NotificationCompany> getNotificationList() {
+		return notificationList;
+	}
+	
+	public void setNotificationList(Stack<NotificationCompany> notificationList) {
+		this.notificationList = notificationList;
+	}
 }
--- Bolsa de empleo Servidor/src/models/Employee.java
+++ Bolsa de empleo Servidor/src/models/Employee.java
@@ -1,5 +1,84 @@
 package models;
 
-public class Employee {
+import java.util.Date;
+import java.util.Stack;
 
+public class Employee extends Account {
+
+	private int idEmployee;
+	private String fistName;
+	private String lastName;
+	private Date birthDate;
+	private String jobTitle;
+	private String professionalProfile;
+	private Stack<NotificationEmployee> notification;
+	
+	public Employee(String email, String password, String namePhoto, String numberPhone, String address, City city,
+			int idEmployee, String fistName, String lastName, Date birthDate, String jobTitle,
+			String professionalProfile, Stack<NotificationEmployee> notification) {
+		super(email, password, namePhoto, numberPhone, address, city);
+		setIdEmployee(idEmployee);
+		setFistName(fistName);
+		setLastName(lastName);
+		setBirthDate(birthDate);
+		setJobTitle(jobTitle);
+		setProfessionalProfile(professionalProfile);
+		setNotification(notification);
+	}
+
+	public int getIdEmployee() {
+		return idEmployee;
+	}
+
+	private void setIdEmployee(int idEmployee) {
+		this.idEmployee = idEmployee;
+	}
+
+	public String getFistName() {
+		return fistName;
+	}
+
+	public void setFistName(String fistName) {
+		this.fistName = fistName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getJobTitle() {
+		return jobTitle;
+	}
+
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+
+	public String getProfessionalProfile() {
+		return professionalProfile;
+	}
+
+	public void setProfessionalProfile(String professionalProfile) {
+		this.professionalProfile = professionalProfile;
+	}
+
+	public Stack<NotificationEmployee> getNotification() {
+		return notification;
+	}
+
+	public void setNotification(Stack<NotificationEmployee> notification) {
+		this.notification = notification;
+	}
 }

88cb79abb0efc8520e802c1db76a7c089b5026f7
julian012 <julicamnet@gmail.com>
2017-12-09 12:51:29 -0500

    Actualizado

--- Bolsa de empleo Servidor/src/models/NotificationCompany.java
+++ Bolsa de empleo Servidor/src/models/NotificationCompany.java
@@ -1,5 +1,35 @@
 package models;
 
-public class NotificationCompany {
+import java.util.Date;
 
+public class NotificationCompany {
+	
+	private int jobOffer;
+	private int idEmployee;
+	private Date date;
+	private StatusJobOffer offer;
+	
+	public NotificationCompany(int jobOffer, int idEmployee, Date date, StatusJobOffer offer) {
+		this.jobOffer = jobOffer;
+		this.idEmployee = idEmployee;
+		this.date = date;
+		this.offer = offer;
+	}
+
+	public int getJobOffer() {
+		return jobOffer;
+	}
+
+	public int getIdEmployee() {
+		return idEmployee;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public StatusJobOffer getOffer() {
+		return offer;
+	}
+	
 }
